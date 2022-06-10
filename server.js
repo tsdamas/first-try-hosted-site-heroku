2c455827ee87482d7cbfcaa0b5af0f2887f4df4c
Thais Damasceno <tssdamasceno@gmail.com>
2022-06-10 08:55:48 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express () // whenever we see app is relating to express
+const PORT = 8000
+
+//create an object to send through the api request
+const rappers = {
+  '21 savage': {
+    'age': 29,
+    'birthName': 'Sheeya',
+    'name': 'London, England' 
+  }, 
+  'chance the rapper': {
+    'age': 28,
+    'birthName': 'John',
+    'name': 'Rio de Janeiro, Brazil ' 
+  }, 
+  'dylon': {
+    'age': 27,
+    'birthName': 'Dylon',
+    'name': 'Sydney, Australia' 
+  }, 
+
+}
+
+//set up the server to generate requests and generate responses 
+//heard a get request and we want to send a response
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html') //dirname means that to look on the dire anstarts from html
+})
+
+//create a api to send a response as json
+//people usually use postman to visualize apis
+//login to postman to check it out
+// to see api that are served that are running locally you have to do something extra:
+//you have to have postman desktop agent running on local machine and once you have it uou can make a request from your localhost
+
+//rappername is a queryparameter
+//the colon mean the parameter that will come
+app.get('/api/:rapperName', (request, response) => {
+  const rapperName = request.params.rapperName.toLowerCase() //this variable will grab the rappername paramater
+  if (rappers[rapperName]){
+    response.json(rappers[rapperName])
+  } else {
+    response.json(rappers['dylon'])
+  }
+  //response.json(rappers)
+
+})
+
+// make the server to listen to the request
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}! You better catch it!`)
+})
+
+//02:40:39
